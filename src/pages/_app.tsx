12f8c69197cddd6a491b89911fda0681bc4e4d0e
PROxZIMA <pratikbpingale9075@gmail.com>
2022-05-24 18:27:06 +0530

    Add: Meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import config from '../../config.json';
 import Head from 'next/head';
 import Script from 'next/script';
 
-
 const App = ({ Component, pageProps }) => {
   const inputRef = React.useRef<HTMLInputElement>(null);
 
@@ -23,6 +22,91 @@ const App = ({ Component, pageProps }) => {
           maximum-scale="1"
         />
         <title>{config.title}</title>
+        <meta name="author" content={config.name} />
+        <meta name="description" content={config.description} />
+
+        <meta property="og:title" content={config.title} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={config.homepage} />
+        <meta property="og:description" content={config.description} />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/PROxZIMA/proxzima.github.io/gh-pages/ogImage.png"
+        />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="./apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="./apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="./apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="./apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="./apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="./apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="./apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="./apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="./apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="./android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="./favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="./favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="./favicon-16x16.png"
+        />
+        <link rel="manifest" href="./manifest.json" />
+        <meta name="msapplication-TileColor" content="#282A36" />
+        <meta name="msapplication-TileImage" content="./ms-icon-144x144.png" />
+        <meta name="theme-color" content="#282A36" />
       </Head>
 
       <Script strategy="beforeInteractive" src="/scripts/darkMode.js" />
