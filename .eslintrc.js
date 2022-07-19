8dfb7b16f0c45183df406a54bbf1b5ec1465b22d
PROxZIMA <pratikbpingale9075@gmail.com>
2022-07-19 23:10:04 +0530

    Refactor: Configure dev setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,19 @@
 module.exports = {
-  plugins: ['prettier'],
+  plugins: ['prettier', 'unused-imports'],
   extends: ['next/core-web-vitals'],
   rules: {
-    'no-console': 'error',
+    'no-console': 'warn',
     'prettier/prettier': 'warn',
     'react-hooks/exhaustive-deps': 'off',
+    'unused-imports/no-unused-imports': 'warn',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
 };
