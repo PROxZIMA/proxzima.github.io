ac6674911d16b66f753ab0b60de4fa001dd227ed
PROxZIMA <pratikbpingale9075@gmail.com>
2022-07-20 23:56:10 +0530

    🚀 Deploy Webpage to gh-pages

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,9 +1,11 @@
 if (
-  localStorage.getItem('proxzimaDarkMode') === 'dark' ||
-  (!('proxzimaDarkMode' in localStorage) &&
-    window.matchMedia('(prefers-color-scheme: dark)').matches)
+  !(
+    localStorage.getItem('proxzimaDarkMode') === 'dark' ||
+    (!('proxzimaDarkMode' in localStorage) &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches)
+  )
 ) {
-  document.documentElement.classList.add('dark');
-} else {
   document.documentElement.classList.remove('dark');
+} else {
+  document.documentElement.classList.add('dark');
 }
