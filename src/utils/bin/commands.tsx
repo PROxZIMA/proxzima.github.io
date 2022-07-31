2d2921ad6cbdb10d1da619b13ba22adca533cae1
PROxZIMA <pratikbpingale9075@gmail.com>
2022-07-31 20:53:52 +0530

    Fix: Multiple sort in help command

--- src/utils/bin/commands.tsx
+++ src/utils/bin/commands.tsx
@@ -4,15 +4,15 @@ import config from '../../../config.json';
 import Banner from '../../components/Banner';
 import * as bin from './index';
 
-// Help
+// Help: Lists all commands
 export const help = async (args: string[]): Promise<string> => {
-  // const commands = Object.keys(bin).sort().join(', ');
-  var c = '';
-  for (let i = 1; i <= Object.keys(bin).sort().length; i++) {
+  const commands = Object.keys(bin).sort();
+  let c = '';
+  for (let i = 1; i <= commands.length; i++) {
     if (i % 7 === 0) {
-      c += Object.keys(bin).sort()[i - 1] + '\n';
+      c += commands[i - 1] + '\n';
     } else {
-      c += Object.keys(bin).sort()[i - 1] + ' ';
+      c += commands[i - 1] + ' ';
     }
   }
   return `Welcome! Here are all the available commands:
